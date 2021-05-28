8b20cd04757fd94f6089f279377ea0057a76b926
heinnssin <heinnssinp@gmail.com>
2021-05-27 22:42:15 -0400

    completed

--- db.js
+++ db.js
@@ -48,25 +48,45 @@ const Student = conn.define('student', {
 
 Lecture.belongsTo(School)
 Lecture.belongsTo(Student)
-// Lecture.belongsTo(Lecture, {as: 'segue'})
+Lecture.belongsTo(Lecture, {as: 'segue'})
 
 Student.hasMany(Lecture)
 School.hasMany(Lecture)
 
 
 
-
 const syncAndSeed = async() => {
   try {
-    await conn.sync( {force: true}),
-    data.schools.map( school => School.create({name: school}))
-    data.lectures.map( lecture => Lecture.create({name: lecture}))
-    data.students.map( student => Student.create({name: student}))
-  //const results  = await Promise.all([schools, teachers, students])
+    await conn.sync( {force: true})
+    const [schools, lectures, students] = await Promise.all([
+       Promise.all(data.schools.map( school =>  School.create({name: school}))),
+       Promise.all(data.lectures.map( lecture =>  Lecture.create({name: lecture}))),
+       Promise.all(data.students.map( student =>  Student.create({name: student})))
+    ])
+    lectures[0].schoolId = schools[0].id
+    lectures[0].studentId = students[0].id
+    lectures[0].segueId = lectures[1].id
+    await lectures[0].save()
+
+    lectures[1].schoolId = schools[1].id
+    lectures[1].studentId = students[1].id
+    lectures[1].segueId = lectures[2].id
+    await lectures[1].save()
+
+    lectures[2].schoolId = schools[3].id
+    lectures[2].studentId = students[3].id
+    lectures[2].segueId = lectures[3].id
+    await lectures[2].save()
+
+    lectures[3].schoolId = schools[1].id
+    lectures[3].studentId = students[1].id
+    lectures[3].segueId = lectures[0].id
+    await lectures[3].save()
+
+
   } catch (ex){
     console.log(ex)
   }
-  
 }
 
 
--- server.js
+++ server.js
@@ -1,22 +1,77 @@
 const express = require('express')
 const app = express();
-// const {conn, syncAndseed, models:{School, Lecture, Student}} = require("./db")
+
 const {
   conn,
   syncAndSeed,
   models: {School, Lecture, Student},
 } = require("./db");
 
+app.get('/lectures', async(req, res, next) => {
+  try {
+    const lectures = await Lecture.findAll({
+      include: {
+        all: true
+      }
+    })
+    //PRACTICE PRINTING LIST
+    // const html  = 
+    // `
+    // <h1>Lectures</h1>
+    // <ul> 
+    // ${lectures.map(lecture => `<li> ${lecture.name} </li> `).join('')}
+    
+    // </ul>
+    
+    // `
+     res.send(lectures)
+   }
+   catch(ex){
+     next(ex)
+   }
+ }
+)
 
 
-// app.use(express.urlencoded({ extened: false }));
+
+app.get('/schools', async(req, res, next) => {
+  try {
+    const schools = await School.findAll({
+      include: {
+        all: true
+      }
+    })
+     res.send(schools)
+   }
+   catch(ex){
+     next(ex)
+   }
+ }
+)
+
+app.get('/students', async(req, res, next) => {
+  try {
+    const students = await Student.findAll({
+      include: {
+        all: true
+      }
+    })
+     res.send(students)
+   }
+   catch(ex){
+     next(ex)
+   }
+ }
+)
 
 app.get("/", async (req, res, next) => {
   try {
-    const schools = await School.findAll()
-    const lectures = await Lecture.findAll()
-    const students = await Student.findAll()
-    res.send(schools)
+   const lectures = await Lecture.findAll({
+     include: {
+       all: true
+     }
+   })
+    res.send(lectures)
   }
   catch(ex){
     next(ex)
@@ -24,15 +79,15 @@ app.get("/", async (req, res, next) => {
 }
 )
 
-const port = 3000
+
 
 const init = async() => {
   try {
     await conn.sync({force: true});
     await syncAndSeed();
-
-    await app.listen(PORT, () => {
-      console.log(`Listening on ${port}`)
+    const port = 3000
+    await app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
     })
   }
   catch(ex){
